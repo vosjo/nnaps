caf139b6acc6aef85ddb88e67c2e51b87650d273
Joris Vos <vos.joris@gmail.com>
2020-01-07 13:35:34 +0100

    Fixed error in defaults that class names of preprocessors were not replaced by their actual class.

--- nnaps/defaults.py
+++ nnaps/defaults.py
@@ -1,5 +1,7 @@
 from sklearn import preprocessing
 
+import copy
+
 default_scaler = preprocessing.StandardScaler
 default_encoder = preprocessing.OneHotEncoder
 
@@ -11,7 +13,32 @@ default_model = [{'layer':'Dense', 'args':[100], 'kwargs': dict(activation='relu
                  {'layer':'Dropout', 'args':[0.1], 'kwargs': dict(name='DO_3')},
                  ]
 
+default_regressor_model = []
+default_classifier_model = []
+
+def get_processor_class(processor_name):
+
+    if processor_name == 'OneHotEncoder':
+        return preprocessing.OneHotEncoder
+
+    elif processor_name == 'StandardScaler':
+        return preprocessing.StandardScaler
+
+    elif processor_name == 'RobustScaler':
+        return preprocessing.RobustScaler
+
+    elif processor_name == 'MinMaxScaler':
+        return preprocessing.MinMaxScaler
+
+    elif processor_name == 'MaxAbsScaler':
+        return preprocessing.MaxAbsScaler
+
+    else:
+        return None
+
 def add_defaults_to_setup(setup):
+
+    setup = copy.deepcopy(setup)
 
     def convert_to_default_dict(setup_dict, key, default_dict):
         setup_dict = setup_dict.copy()
@@ -30,11 +57,24 @@ def add_defaults_to_setup(setup):
             for f in setup_dict[key].keys():
                 if def_key not in setup_dict[key][f]:
                     setup_dict[key][f][def_key] = def_val
+                else:
+                    setup_dict[key][f][def_key] = get_processor_class(setup_dict[key][f][def_key])
 
         return setup_dict
 
+    # check and update the preprocessors for features and regressors/classifiers
     setup = convert_to_default_dict(setup, 'features', {'processor': default_scaler})
     setup = convert_to_default_dict(setup, 'regressors', {'processor': None})
     setup = convert_to_default_dict(setup, 'classifiers', {'processor': default_encoder})
+
+    # check and update the model setup
+    if not 'model' in setup:
+        setup['model'] = default_model
+
+    if not 'regressor_model' in setup:
+        setup['regressor_model'] = default_regressor_model
+
+    if not 'classifier_model' in setup:
+        setup['classifier_model'] = default_classifier_model
 
     return setup
--- nnaps/tests/test_defaults.py
+++ nnaps/tests/test_defaults.py
@@ -5,6 +5,26 @@ from nnaps import defaults
 
 class TestDefaultsGenerator(unittest.TestCase):
 
+    def test_get_processor_class(self):
+
+        self.assertEqual(defaults.get_processor_class('OneHotEncoder')().__class__.__name__,
+                         'OneHotEncoder')
+
+        self.assertEqual(defaults.get_processor_class('StandardScaler')().__class__.__name__,
+                         'StandardScaler')
+
+        self.assertEqual(defaults.get_processor_class('RobustScaler')().__class__.__name__,
+                         'RobustScaler')
+
+        self.assertEqual(defaults.get_processor_class('MinMaxScaler')().__class__.__name__,
+                         'MinMaxScaler')
+
+        self.assertEqual(defaults.get_processor_class('MaxAbsScaler')().__class__.__name__,
+                         'MaxAbsScaler')
+
+        self.assertEqual(defaults.get_processor_class('FakeEncoder'), None)
+
+
     def test_add_defaults_to_setup(self):
         setup = {'features': ['M1', 'qinit', 'Pinit', 'FeHinit'],
                  'regressors': ['Pfinal', 'qfinal'],
@@ -20,3 +40,38 @@ class TestDefaultsGenerator(unittest.TestCase):
 
         self.assertTrue('processor' in setup_new['classifiers']['product'])
         self.assertEqual(setup_new['classifiers']['product']['processor'], defaults.default_encoder)
+
+
+    def test_default_preprocessor_update(self):
+
+        setup = {'features':  {'M1': {'processor': 'StandardScaler'},
+                               'qinit': {'processor': 'RobustScaler'},
+                               'Pinit': {'processor': 'MinMaxScaler'},
+                               'FeHinit': {'processor': 'MaxAbsScaler'},},
+
+                 'regressors': ['Pfinal', 'qfinal'],
+
+                 'classifiers': {'binary_type': {'processor': None},
+                                 'product': {'processor': 'OneHotEncoder'},},
+                 }
+
+        setup_new = defaults.add_defaults_to_setup(setup)
+
+        for key in setup['features'].keys():
+            self.assertEqual(setup_new['features'][key]['processor'].__name__, setup['features'][key]['processor'],
+                             msg="{} does not have preprocessor with correct class.".format(key) +
+                                 " got {}, expected {}".format(setup_new['features'][key]['processor'].__name__,
+                                                              setup['features'][key]['processor']))
+
+        for key in setup['regressors']:
+            self.assertEqual(setup_new['regressors'][key]['processor'], None,
+                             msg="{} should not have a preprocessor.".format(key) +
+                                 " got {}, expected {}".format(setup_new['regressors'][key]['processor'], None))
+
+        self.assertEqual(setup_new['classifiers']['binary_type']['processor'], None,
+                         msg="{} should not have a preprocessor.".format('binary_type') +
+                             " got {}, expected {}".format(setup_new['classifiers']['binary_type']['processor'], None))
+
+        self.assertEqual(setup_new['classifiers']['product']['processor'].__name__, 'OneHotEncoder',
+                         msg="{} does not have preprocessor with correct class.".format('product') +
+                             " got {}, expected OneHotEncoder".format(setup_new['classifiers']['product']['processor'].__name__))
