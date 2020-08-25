9abc564e5756d9c357d09a7d507a4a3d75b2b91a
Joris Vos <vos.joris@gmail.com>
2020-08-25 14:31:37 +0200

    Added documentation

--- nnaps/tests/conftest.py
+++ nnaps/tests/conftest.py
@@ -4,4 +4,8 @@ import pytest
 
 @pytest.fixture
 def root_dir():
+    """
+    We need to use the os package when providing file paths for loading saved models in the FCpredictor and XGB
+    predictor. For unknown reasons these don't work with the Path library.
+    """
     return os.path.dirname(os.path.abspath(__file__))
