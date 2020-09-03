7b65dc759a1ca58918d6a83a675ddcdccf444b68
Joris Vos <vos.joris@gmail.com>
2020-09-03 15:16:49 +0200

    futher work on gui

--- nnaps/mesa/main.py
+++ nnaps/mesa/main.py
@@ -23,43 +23,157 @@ def get_file_list(input_dirs):
 
     return file_list
 
+
+def compress(inputdir, setupfile, inputfile, outputdir, skip=False):
+    # run the convert2hdf5 function
+
+    print ("Compress", inputdir, setupfile, inputfile, outputdir, skip)
+
+    # if output is None:
+    #     print("You need to specify an output path with option -o <path>")
+    #     exit()
+    #
+    # # if necessary load default setup. Check local folder first, then system defaults, then load from defaults file
+    # if setup is None:
+    #     if os.path.isfile('default_2h5.yaml'):
+    #         setup = defaults.read_defaults('default_2h5.yaml')
+    #     elif os.path.isfile('~/.nnaps/default_2h5.yaml'):
+    #         setup = defaults.read_defaults('~/.nnaps/default_2h5.yaml')
+    #     else:
+    #         setup = defaults.defaults_2h5
+    # else:
+    #     setup = defaults.read_defaults(setup)
+    #
+    # modelfile = Path(input[0])
+    #
+    # if modelfile.is_dir():
+    #     # check if the path given is of 1 MESA model, or if it contains multiple directories with MESA models
+    #     # check is performed by checking if a modelfile/inlist file exist
+    #
+    #     if (modelfile / 'inlist').is_file():
+    #         model_list = [modelfile]
+    #         model_list = pd.DataFrame(data={'path': [p.name for p in model_list]})
+    #         setup['input_path_prefix'] = ''
+    #         setup['input_path_kw'] = 'path'
+    #     else:
+    #         model_list = modelfile.glob('*')
+    #         model_list = pd.DataFrame(data={'path': [p.name for p in model_list]})
+    #         setup['input_path_prefix'] = args.toh5[0]
+    #         setup['input_path_kw'] = 'path'
+    # else:
+    #     model_list = pd.read_csv(input)
+    #     if len(input) > 1:
+    #         setup['input_path_prefix'] = input[1]
+    #
+    # read_mesa.convert2hdf5(model_list, output_path=outputdir, **setup, skip_existing=skip, verbose=True)
+    #
+    # print("--> {}".format(outputdir))
+
+def extract(inputdir, setupfile, outputfile):
+
+    print ("Extract", inputdir, setupfile, outputfile)
+
+    # if setupfile is None:
+    #
+    #     if os.path.isfile('default_extract.yaml'):
+    #         setup = defaults.read_defaults('default_extract.yaml')
+    #     elif os.path.isfile('~/.nnaps/default_extract.yaml'):
+    #         setup = defaults.read_defaults('~/.nnaps/default_extract.yaml')
+    #     else:
+    #         setup = defaults.default_extract
+    #
+    # else:
+    #     setup = defaults.read_defaults(setupfile)
+    #
+    # # check which output filename to use
+    # if 'output' not in setup and outputfile is None:
+    #     print("You need to specify an output file with option -o <filename>")
+    #     exit()
+    # else:
+    #     if outputfile is not None:
+    #         output = outputfile
+    #     else:
+    #         output = setup['output']
+    #
+    # # check which input directories to use
+    # if 'input' in setup and len(inputdir) == 0:
+    #     file_list = get_file_list(setup['input'])
+    # else:
+    #     file_list = get_file_list(inputdir)
+    #
+    # if len(file_list) == 0:
+    #     print("No input files found!")
+    #     exit()
+    # else:
+    #     print('Found {} files'.format(len(file_list)))
+    #
+    # result = extract_mesa.extract_mesa(file_list, **setup, verbose=True)
+    #
+    # result.to_csv(output, index=False, na_rep='NaN')
+    #
+    # print("--> {}".format(output))
+
 @Gooey(advanced=True,
        required_cols=1,  # number of columns in the "Required" section
        optional_cols=1,  # number of columns in the "Optional" section
        )
-def main_gui():
+def main():
     parser = GooeyParser(description='NNaPS-mesa: Process MESA models')
 
-    subparsers = parser.add_subparsers()
+    subparsers = parser.add_subparsers(dest='action')
 
-    compress_group = subparsers.add_parser('Compress', help='Compress a grid of MESA models')
-    compress_group.add_argument('input', default=None,
-                        help='Directory containing the MESA models to process', widget='DirChooser')
-    compress_group.add_argument('setup', default=None,
+    #--Compress--
+
+    compress_parser = subparsers.add_parser('Compress', help='Compress a grid of MESA models')
+
+    input_group = compress_parser.add_argument_group("Input")
+
+    input_group.add_argument('inputdir', default=None,
+                        help='Directory containing the MESA models to process',
+                        widget='DirChooser')
+    input_group.add_argument('-inputfile', default=None,
+                        help='CSV file containing extra info for models to process',
+                        widget='FileChooser')
+    input_group.add_argument('setupfile', default=None,
                         help='The setup file containing necessary settings',
                         widget='FileChooser')
-    compress_group.add_argument('output', default=None,
+    input_group.add_argument('outputdir', default=None,
                         help='The output directory to store the compressed models',
                         widget='DirChooser')
-    compress_group.add_argument('--skip', dest='skip', default=False, action='store_true',
+
+    option_group = compress_parser.add_argument_group("Options")
+
+    option_group.add_argument('--skip', dest='skip', default=False, action='store_true',
                                 help='skip models that have already been transformed to h5.')
 
+    #--Extract--
 
-    extract_group = subparsers.add_parser("Extract",
+    extract_parser = subparsers.add_parser("Extract",
                                         help="Extract aggregate parameters from a grid of compressed MESA models")
-    extract_group.add_argument('-extract', dest='extract', default=None, nargs='*',
-                        help='Extract parameters from history files stored as h5')
-    extract_group.add_argument('setup', default=None,
-                        help='The setup file containing necessary info for the -2h5 and -extract option')
-    extract_group.add_argument('-o_extract', dest='extract_output', default=None,
-                        help='The output file or directory for the -2h5 and -extract functions')
+
+    input_group_2 = extract_parser.add_argument_group("Input")
+
+    input_group_2.add_argument('-inputdir', default=None,
+                        help='Directory containing the compressed MESA models to process',
+                        widget='DirChooser')
+    input_group_2.add_argument('setupfile', default=None,
+                        help='The setup file containing necessary settings',
+                        widget='FileChooser')
+    input_group_2.add_argument('-outputfile', default=None,
+                        help='The output csv file to store the extracted parameters')
+
     args = parser.parse_args()
 
-    pass
+    if args.action == 'Compress':
+
+        compress(args.inputdir, args.setupfile, args.inputfile, args.outputdir, skip=args.skip)
+
+    else:
+
+        extract(args.inputdir, args.setupfile, args.outputfile)
 
 
-
-def main():
+def main_old():
     parser = argparse.ArgumentParser(description='NNaPS-mesa: Process MESA models')
     group = parser.add_mutually_exclusive_group()
     group.add_argument('-2h5', dest='toh5', nargs='+', default=None,
@@ -142,7 +256,7 @@ def main():
             else:
                 output = setup['output']
 
-        # check which intput directories to use
+        # check which input directories to use
         if 'input' in setup and len(args.extract) == 0:
             file_list = get_file_list(setup['input'])
         else:
