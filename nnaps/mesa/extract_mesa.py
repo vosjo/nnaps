0ace5f5e9f3bde9f3f7e44f57f423824cd2512b8
Joris Vos <vos.joris@gmail.com>
2020-10-02 15:20:57 +0200

    Improved parameter checking in file_list

--- nnaps/mesa/extract_mesa.py
+++ nnaps/mesa/extract_mesa.py
@@ -43,6 +43,35 @@ def extract_parameters(data, parameters=[], phase_flags=[]):
     return result
 
 
+def process_file_list(file_list, **kwargs):
+    """
+    Function that takes a list containing the paths of the mesa models to process, and potentially some relevant
+    parameters necessary for the processing. Check if the setup parameters provided in the kwargs are included in
+    the list, and if not, add them.
+
+    if ce_parameters is included in the file_list, it will convert the provided ce_parameters to dictionary.
+
+    :param file_list: Pandas DataFrame containing at least 1 column with the path of the MESA models to process
+    :param kwargs: all parameters that are required for processing the MESA models and their default values.
+    :return: Pandas DataFrame containing the path to all models and the necessary parameters.
+    """
+
+    ce_parameters = kwargs.pop('ce_parameters', None)
+
+    # add extraction parameters to the file_list
+    for setup_par in kwargs.keys():
+        if setup_par not in file_list.columns:
+            file_list[setup_par] = kwargs[setup_par]
+
+    # ce_parameters is treated separately because it should be converted from string to dictionary if already included
+    if ce_parameters is not None and 'ce_parameters' not in file_list.columns:
+        file_list['ce_parameters'] = [ce_parameters for i in file_list['path']]
+    else:
+        file_list['ce_parameters'] = [eval(p) for p in file_list['ce_parameters']]
+
+    return file_list
+
+
 def extract_mesa(file_list, stability_criterion='J_div_Jdot_div_P', stability_limit=10,
                  ce_formalism='iben_tutukov1984', ce_parameters={'al':1}, ce_profile_name=None,
                  parameters=[], phase_flags=[], extra_info_parameters=[], verbose=False,**kwargs):
@@ -72,10 +101,8 @@ def extract_mesa(file_list, stability_criterion='J_div_Jdot_div_P', stability_li
 
     # check if the same extraction parameters are used for all models, or if specific parameters are already
     # provided in the files list
-    for setup_par in ['stability_criterion', 'stability_limit', 'ce_formalism', 'ce_profile_name']:
-        if setup_par not in file_list.columns:
-            file_list[setup_par] = locals()[setup_par]
-    file_list['ce_parameters'] = [ce_parameters for i in file_list['path']]
+    file_list = process_file_list(file_list, stability_criterion=stability_criterion, stability_limit=stability_limit,
+                 ce_formalism=ce_formalism, ce_parameters=ce_parameters, ce_profile_name=ce_profile_name)
 
     for i, model in file_list.iterrows():
 
